69e88fe89ef51480bc6b2444c7b94293a20aff7b
jzanchi2026 <147829833+jzanchi2026@users.noreply.github.com>
2025-06-24 17:04:36 -0400

    First test added

--- core/src/test/kotlin/org/iks/kriegsspiel/ModelTest.kt
+++ core/src/test/kotlin/org/iks/kriegsspiel/ModelTest.kt
@@ -3,14 +3,23 @@ package org.iks.kriegsspiel
 import com.badlogic.gdx.graphics.Texture
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.*
+import java.time.LocalDateTime
 import kotlin.test.Test
 
 class ModelTest {
-    private val testPiece = Piece(0f, 0f, 100f, 100f, mockk<Texture>())
-    private val testGameState = GameState(listOf(testPiece))
-    private val testModel = Model(testGameState)
+
 
     @Test
     fun testRecomputeState() {
+        val testPiece = Piece(0f, 0f, 100f, 100f, mockk<Texture>())
+        val testGameState = GameState(listOf(testPiece))
+        val testModel = Model(testGameState)
+        val testPiece2 = Piece(10f, 10f, 100f, 100f, mockk<Texture>())
+        val testChange = PieceUpdate(testGameState.pieces, 0, testPiece2, LocalDateTime.now())
+
+        testModel.updates += testChange
+        testModel.recomputeCurrentState()
+
+        assertEquals(testModel.currentState.pieces[0], testPiece2)
     }
 }
